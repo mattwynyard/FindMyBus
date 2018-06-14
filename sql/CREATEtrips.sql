ea1fceaa9f84ffc47c34ba3a16051d24fbc512d8
mjwynyard@gmail.com <matt@Titanium.local>
2018-06-14 18:56:08 +1200

    Intial commit

---
+++ sql/CREATEtrips.sql
@@ -0,0 +1,13 @@
+create table trips (
+                    trip_id varchar(50),
+                    route_id varchar(50),
+                    service_id varchar(50),
+                    trip_headsign varchar(50),
+                    direction_id smallint,
+                    block_id varchar(50),
+                    shape_id varchar(50),
+                    trip_short_name varchar(20),
+                    trip_type varchar(50),
+                    PRIMARY KEY (trip_id),
+                    FOREIGN KEY (route_id) REFERENCES routes (route_id)
+);
