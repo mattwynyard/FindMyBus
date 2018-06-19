185d139dda903328956ce18bd081319fde5c71e9
mattwynyard <mjwynyard@gmail.com>
2018-06-20 11:54:22 +1200

    Server running -> serves bus JSON by number

--- server.js
+++ server.js
@@ -4,22 +4,39 @@ server = http.createServer( function(req, res) {
 
     if (req.method == 'POST') {
         console.log("POST");
-        var body = '';
+        var body= '';
         req.on('data', function (data) {
             body += data;
             //console.log("Partial body: " + body);
         });
         req.on('end', function () {
-            console.log("Body: " + body);
+            var myJSON = {latitude: '',longitude: ''};
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            var html = fs.readFileSync('bus.json', 'utf-8');
+            html = JSON.parse(html);
+            var arr = [];
+            
+            console.log(body.substring(4, body.length));
+            var bus = body.substring(4, body.length);
+            bus = bus.toUpperCase();
+            for (var i = 0; i < html.length; i += 1) {
+                var myJSON = {bus: '', vehicle_id: '', latitude: '',longitude: '', bearing: '',direction_id: ''} ;
+                if (html[i].route_short_name == bus) {
+                    myJSON.bus = html[i].route_short_name;
+                    myJSON.vehicle_id = html[i].vehicle_id;
+                    myJSON.latitude = html[i].latitude;
+                    myJSON.longitude = html[i].longitude;
+                    myJSON.bearing = html[i].bearing;
+                    myJSON.direction_id = html[i].direction_id;
+                    arr.push(myJSON);
+                }
+            }
+        res.end(JSON.stringify(arr));
         });
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        var html = fs.readFileSync('json/positions.json', 'utf-8');
-        res.end(html);
     }
     else
     {
         console.log("GET");
-        //var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
         var html = fs.readFileSync('index.html');
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(html);
