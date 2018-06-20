0eaa605ceff41271f911fd9f91c7d3a99b9bee1a
mattwynyard <mjwynyard@gmail.com>
2018-06-21 09:49:16 +1200

    added support for lan

--- server.js
+++ server.js
@@ -1,12 +1,67 @@
-//"use strict";
-const http = require('http');
-let port = process.env.PORT || 3000;
-let host = '127.0.0.1';
-//var express = require('express');
-const app = require('./app');
+"use strict";
 
-http.createServer(app).listen(port);
-console.log('Listening at http://' + host + ':' + port);
+var server = (function() {
 
-//module.exports = app
+    const http = require('http');
+    const port = process.env.PORT || 3000;
+    const os = require('os');
+    const ifaces = os.networkInterfaces();
+    var address = '127.0.0.1'; //getIPAddress();
+    const app = require('./app'); //import app.js
+    const server = http.createServer(app);
+
+    function useLAN(val) {
+        if (val === true) {
+            address = getIPAddress();
+            server.listen(3000, address);
+            console.log('Listening at http://' + address + ':' + port);
+        } else {
+            server.listen(3000, address);
+            console.log('Listening at http://' + address + ':' + port);
+        }
+    }
+
+    function getIPAddress() {
+        var addr = '';
+        Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
+
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+            }
+
+            if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+            //console.log(ifname + ':' + alias, iface.address);
+            } else {
+            // this interface has only one ipv4 adress
+            //console.log(ifname, iface.address);
+            addr = iface.address;
+            }
+            ++alias;
+        });
+        
+        });
+        console.log(addr);
+        return addr;;
+    } //end getIPAddress()
+    return {
+        useLAN: useLAN
+    };
+})();//end closure
+
+process.argv.forEach((val, index) => {
+    console.log(val);
+    if (index == 2 && val == "-lan") {
+        server.useLAN(true);
+    } else if (index == 2 && val == "-local") {
+        server.useLAN(false);
+    } else {
+        return 0;
+    }
+});
+
+
 
