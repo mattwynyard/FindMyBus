322aca4b28a77680e352d0add7d2d6771f325de1
mattwynyard <mjwynyard@gmail.com>
2018-06-20 12:16:44 +1200

    added query to extract json from db

--- model/sql/findBus.sql
+++ model/sql/findBus.sql
@@ -1,8 +1,10 @@
-SELECT p.vehicle_id, p.latitude, p.longitude
-FROM routes r, positions p
-WHERE r.route_id = p.route_id and r.route_short_name='595';
-copy(
-SELECT p.vehicle_id, p.latitude, p.longitude, p.bearing
-FROM routes r, positions p
-WHERE r.route_id = p.route_id and r.route_short_name='595')
-to '/Users/matt/FindMyBus/positions.csv' With CSV DELIMITER ',';
+--SELECT r.route_short_name, p.vehicle_id, p.latitude, p.longitude, p.bearing, t.direction_id
+--FROM routes r, positions p, trips t
+--WHERE r.route_id = p.route_id and p.trip_id = t.trip_id and r.route_short_name='112';
+copy (
+SELECT json_agg(row_to_json(t))
+FROM (SELECT r.route_short_name, p.vehicle_id, p.latitude, p.longitude, p.bearing, t.direction_id
+FROM routes r, positions p, trips t
+WHERE r.route_id = p.route_id and p.trip_id = t.trip_id) t --and t.direction_id=)
+)
+to '/Users/matt/FindMyBus/bus.json';
---
+++ model/sql/findBus_number.sql
@@ -0,0 +1,10 @@
+SELECT r.route_short_name, p.vehicle_id, p.latitude, p.longitude, p.bearing, t.direction_id
+FROM routes r, positions p, trips t
+WHERE r.route_id = p.route_id and p.trip_id = t.trip_id and r.route_short_name='112';
+-- copy (
+-- SELECT json_agg(row_to_json(t))
+-- FROM (SELECT r.route_short_name, p.vehicle_id, p.latitude, p.longitude, p.bearing, t.direction_id
+-- FROM routes r, positions p, trips t
+-- WHERE r.route_id = p.route_id and p.trip_id = t.trip_id and r.route_short_name='112') t
+-- )
+-- to '/Users/matt/FindMyBus/bus.json';
