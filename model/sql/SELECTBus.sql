59f3acd7fc5e72181ca941bb6abbef3a6c79db17
mattwynyard <mjwynyard@gmail.com>
2018-06-20 19:44:30 +1200

    moved model into api folder

--- model/sql/SELECTBus.sql
+++
@@ -1,5 +0,0 @@
-SELECT p.vehicle_id, p.latitude, p.longitude, p.bearing, t.direction_id
-FROM routes r, trips t, positions p
-WHERE r.route_id=t.route_id and t.trip_id = p.trip_id and r.route_short_name='112';
-
---and t.direction_id = 0 
