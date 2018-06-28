4166f27fb4ce15a8454cba6e67dc99ea81f29994
mattwynyard <mjwynyard@gmail.com>
2018-06-28 13:09:53 +1200

    runs on continual loop now

--- shell/client_light.sh
+++ shell/client_light.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-echo "client running...."
-node ./node/client.js positions
-echo "positions:"
-psql findmybus -f /Users/matt/FindMyBus/api/model/sql/INSERTpositions.sql
-echo "updating locations"
-psql findmybus -f /Users/matt/FindMyBus/api/model/sql/findBus.sql
+while [ 1 = 1 ]; do
+    echo "client running...."
+    node ./node/client.js positions
+    echo "positions:"
+    psql findmybus -f /Users/matt/FindMyBus/api/model/sql/INSERTpositions.sql
+    echo "updating locations"
+    psql findmybus -f /Users/matt/FindMyBus/api/model/sql/findBus.sql
+    sleep 10
+done
 
