a10d083a8d2cd33fc1565a79b5e5b52a586b69c3
mattwynyard <mjwynyard@gmail.com>
2018-06-28 13:05:52 +1200

    added stops route

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 const routes = require('./api/routes/positions')
+const stops = require('./api/routes/stops')
 
 app.use('/positions', routes);
+app.use('/stops', stops);
+
+// app.use((req, res, next) => {
+//     const error = new Error('Not found');
+//     error.status(404);
+//     next(error);
+// });
+
+// app.use((err, req, res, next) => {
+//     res.status(404);
+//     next(err.status || 500);
+// });
 
 module.exports = app;
