0c83d9be2563647bc9b0f23f10c6dbe9171078e7
mattwynyard <mjwynyard@gmail.com>
2018-07-04 10:51:35 +1200

    Added shapes to calls, fixed db rebuild script

--- shell/buildDB.sh
+++ shell/buildDB.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 psql findmybus -f /Users/matt/FindMyBus/model/sql/buildDB.sql
+echo "rebuilt db"
+echo "client running...."
+node ./node/client.js routes
+echo "routes:"
+psql findmybus -f /Users/matt/FindMyBus/api/model/sql/INSERTroutes.sql
+node ./node/client.js trips
+echo "trips:"
+psql findmybus -f /Users/matt/FindMyBus/api/model/sql/INSERTtrips.sql
+node ./node/client.js positions
+echo "positions:"
+psql findmybus -f /Users/matt/FindMyBus/api/model/sql/INSERTpositions.sql
+echo "shapes:"
+psql findmybus -f /Users/matt/FindMyBus/api/model/sql/SELECTshapes.sql
+echo "updating locations"
+psql findmybus -f /Users/matt/FindMyBus/api/model/sql/findBus.sql
--- shell/client.sh
+++ shell/client.sh
@@ -9,6 +9,8 @@ psql findmybus -f /Users/matt/FindMyBus/api/model/sql/INSERTtrips.sql
 node ./node/client.js positions
 echo "positions:"
 psql findmybus -f /Users/matt/FindMyBus/api/model/sql/INSERTpositions.sql
+echo "shapes:"
+psql findmybus -f /Users/matt/FindMyBus/api/model/sql/SELECTshapes.sql
 echo "updating locations"
 psql findmybus -f /Users/matt/FindMyBus/api/model/sql/findBus.sql
 
