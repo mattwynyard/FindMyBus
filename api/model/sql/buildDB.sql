4e9b45d24b9c69344d28cf0b698c4a0fbfa34216
mattwynyard <mjwynyard@gmail.com>
2018-07-05 11:39:13 +1200

    added route_id to createshapes.sql

--- api/model/sql/buildDB.sql
+++ api/model/sql/buildDB.sql
@@ -57,3 +57,15 @@ create table trip_updates (
                     PRIMARY KEY (vehicle_id),
                     FOREIGN KEY (trip_id) REFERENCES trips(trip_id)
 );
+
+create table shapes (
+                    shape_id varchar(50),
+                    shape_pt_lat real,
+                    shape_pt_long real,
+                    shape_pt_sequence int,
+                    shape_dist_traveled real,
+                    route_id varchar(50),
+
+                    PRIMARY KEY (shape_id, shape_pt_sequence),
+                    FOREIGN KEY (route_id) REFERENCES routes (route_id)
+);
