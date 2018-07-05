aab50008ff47fd8137519ecaaa929ec3cdbd967e
mattwynyard <mjwynyard@gmail.com>
2018-07-05 20:55:26 +1200

    Added some debug output

--- api/model/json/fileChunker.js
+++ api/model/json/fileChunker.js
@@ -8,15 +8,20 @@ try {
     var dataString = []
     for (var i = 0; i <= data.length; i += 1) {
         dataString.push(data[i]);
-        if (i % 10000 == 0 && i != 0) {
+        if (i % 10000 == 0 && i != 0) {    
+            console.log(dataString.length);
+            console.log('writing.. ./filechunks/shapes_routes' + (i / 10000) + '.json');
             fs.writeFileSync('./filechunks/shapes_routes' + (i / 10000) + '.json', JSON.stringify(dataString));
             dataString = [];
-        } else if (i == data.length) {
+        } else if(i == data.length - 1) { //last chunk
+            console.log(dataString.length);
+            console.log('./filechunks/shapes_routes' + (Math.ceil(i / 10000)) + '.json');
             fs.writeFileSync('./filechunks/shapes_routes' + (Math.ceil(i / 10000)) + '.json', JSON.stringify(dataString));
         } else {
             continue;
         }
     }
 } catch(e) {
+
     console.log('Error:', e.stack);
 }
